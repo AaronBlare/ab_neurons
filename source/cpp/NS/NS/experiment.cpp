6e82faa5f949a94a685fe99b9d93b4157ab0a70c
Unknown <hewitt.archie@yandex.ru>
2018-02-10 21:53:05 +0300

    Fixed multiple fout calc

--- source/cpp/NS/NS/experiment.cpp
+++ source/cpp/NS/NS/experiment.cpp
@@ -57,16 +57,16 @@ void basic_exp(RunParam * rp, ConfigParam * cp)
 					write_2d_double_data(fn_data_env, md->data_env_evo, md->size_env, md->size_evo, 16, 0);
 				}
 
+				calc_f_out(rp, cp, md);
+
 				for (int n_id = 0; n_id < cp->nn; n_id++)
 				{
-
 					string fn_data_neu = rp->path + "data_neu_" + to_string(n_id) + file_name_suffix(rp, cp, 4);
 					write_2d_double_data(fn_data_neu, md->data_neu_evo[n_id], md->size_neu, md->size_evo, 16, 0);
 
 					string fn_time = rp->path + "time_" + to_string(n_id) + file_name_suffix(rp, cp, 4);
 					write_double_data(fn_time, md->time_evo, md->size_evo, 16, 0);
 
-					calc_f_out(rp, cp, md);
 					string fn_f_out = rp->path + "f_out_" + to_string(n_id) + file_name_suffix(rp, cp, 4);
 					write_double_data(fn_f_out, &(md->f_out[n_id]), 1, 16, 0);
 				}
